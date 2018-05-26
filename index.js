1d2bb9cd81363239a017b10189b25925c1327ad3
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-26 15:39:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function Scooter(year, color, model) {
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+function Driver(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+function PickupLocation(address, city, desc) {
+  this.address = address
+  this.city = city
+  this.desc = desc
+}
